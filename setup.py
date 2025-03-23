2caf1527d3a31b25055a04c66ee59397235c0a7c
A'eala <zenforic@users.noreply.github.com>
2025-03-23 07:22:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     requirements = [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
 setup(
-    name='csm',
+    name='csm-multi',
     version='0.1.0',
     packages=find_packages(),
     install_requires=requirements,
